eed6687442e02adb4f94f5528515b0b62ae5c595
555Jeka555 <2003antyshev@mail.ru>
2024-09-25 15:28:37 +0300

    Написал первое приблежение перевода из мили в мура

--- lab1/MillyMur/main.py
+++ lab1/MillyMur/main.py
@@ -1,19 +1,121 @@
 import csv
 
+NEW_STATE_NAME = 'q'
+
 
 def readMilyFromCsv(fileName, delimiter=';'):
     with open(fileName, 'r', encoding='ISO-8859-1') as file:
         reader = csv.reader(file, delimiter=delimiter)
+        data = []
+
+        states = []
+        newStates = []
+        inputValues = []
+        newStateCount = 0
+        mureStates = {}
 
         for row in reader:
+            data.append(row)
+
+        for row in data:
             print(row)
+        print()
+
+        i = 0
+        for row in data:
+            j = 0
+            for cell in row:
+                if i == 0 and j != 0:
+                    states.append(cell)
+                elif i != 0 and j == 0:
+                    inputValues.append(cell)
+                elif i != 0 and j != 0:
+                    newStateName = NEW_STATE_NAME + str(newStateCount)
+                    newStateCount += 1
+
+                    newStateAlreadyExist = False
+                    for key in mureStates.keys():
+                        if key == cell:
+                            newStateAlreadyExist = True
+                    if newStateAlreadyExist:
+                        continue
+
+                    mureStates[cell] = newStateName
+                    newStates.append(newStateName)
+                j += 1
+            i += 1
+
+        millyInputValue = {}
+        i = 0
+        for row in data:
+            if i == 0:
+                i += 1
+                continue
+
+            currentInputValue = ''
+            j = 0
+            for cell in row:
+                if j == 0:
+                    currentInputValue = cell
+                elif j != 0:
+                    currentState = states[j - 1]
+
+                    if currentInputValue not in millyInputValue:
+                        millyInputValue[currentInputValue] = {}
+
+                    millyInputValue[currentInputValue][currentState] = mureStates[cell]
+
+                j += 1
+
+            i += 1
+
+        return inputValues, mureStates, millyInputValue
 
 
-def writeMureToCsv(fileName, delimiter=';'):
+def writeMureToCsv(fileName, data, delimiter=';'):
     with open(fileName, 'w', newline='', encoding='ISO-8859-1') as file:
         writer = csv.writer(file, delimiter=delimiter)
-        writer.writerows([])
+        writer.writerows(data)
 
 
 if __name__ == '__main__':
-    readMilyFromCsv("data/read/Milly1.csv")
+
+    inputValues, mureStates, millyInputValue = readMilyFromCsv("data/read/Milly1.csv")
+
+    data = []
+
+    print("inputValues: ", inputValues)
+    print("mureStates: ", mureStates)
+    print("millyInputValue: ", millyInputValue)
+
+    width = len(mureStates.values()) + 1
+    height = len(inputValues) + 2
+
+    for _ in range(height):
+        tmp = []
+        for _ in range(width):
+            tmp.append('')
+
+        data.append(tmp)
+
+    i = 0
+    for stateWithOutValue, newState in mureStates.items():
+        data[0][i + 1] = stateWithOutValue
+        data[1][i + 1] = newState
+
+        j = 0
+        for inputValue in inputValues:
+            data[j + 2][0] = inputValue
+
+            statesToNewState = millyInputValue[inputValue]
+            for state, newStateFromMilly in statesToNewState.items():
+                if state in stateWithOutValue:
+                    data[j + 2][i + 1] = newStateFromMilly
+
+            j += 1
+        i += 1
+
+    for row in data:
+        print(row)
+
+    writeMureToCsv("data/write/Milly1.csv", data)
