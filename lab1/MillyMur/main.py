fe77164007d8aed814fddf76972672335015b076
555Jeka555 <2003antyshev@mail.ru>
2024-09-29 14:55:05 +0300

    Написал перевод из мура в милли

--- lab1/MillyMur/main.py
+++ lab1/MillyMur/main.py
@@ -83,12 +83,7 @@ def writeToCsv(fileName, data, delimiter=';'):
         writer.writerows(data)
 
 
-def convertMillyToMure(inputValues, mureStates, v, millyInputValue):
-    print("inputValues: ", inputValues)
-    print("mureStates: ", mureStates)
-    print("millyStates: ", millyStates)
-    print("millyInputValue: ", millyInputValue)
-
+def convertMillyToMure(inputValues, mureStates, millyStates, millyInputValue):
     data = []
     width = len(mureStates.values()) + len(millyStates) + 1
     height = len(inputValues) + 2
@@ -98,7 +93,6 @@ def convertMillyToMure(inputValues, mureStates, v, millyInputValue):
         tmp = []
         for _ in range(width):
             tmp.append('')
-
         data.append(tmp)
 
     i = 0
@@ -153,14 +147,111 @@ def convertMillyToMure(inputValues, mureStates, v, millyInputValue):
     return data
 
 
+def readMureFromCsv(fileName, delimiter=';'):
+    with open(fileName, 'r', encoding='ISO-8859-1') as file:
+        reader = csv.reader(file, delimiter=delimiter)
+        data = []
+
+        inputValues = []
+        mureStates = {}
+
+        for row in reader:
+            data.append(row)
+
+        printFormattedDict(data)
+
+        i = 0
+        for row in data:
+            j = 0
+
+            for cell in row:
+                if i > 1 and j == 0:
+                    inputValues.append(cell)
+                elif i > 1 and j != 0:
+                    millyStateWithOut = data[0][j]
+                    mureState = data[1][j]
+
+                    if len(millyStateWithOut) == 0 and len(mureState) == 0:
+                        continue
+
+                    mureStates[mureState] = millyStateWithOut
+
+                j += 1
+            i += 1
+
+        millyInputValues = {}
+        i = 0
+        for row in data:
+            if i <= 1:
+                i += 1
+                continue
+
+            currentInputValue = ''
+            j = 0
+            for cell in row:
+                if j == 0:
+                    currentInputValue = cell
+                elif j != 0:
+                    currentState = data[0][j].split('/')[0]
+                    if len(currentState) == 0:
+                        continue
+
+                    if currentState not in millyInputValues:
+                        millyInputValues[currentState] = {}
+
+                    millyInputValues[currentState][currentInputValue] = mureStates[cell]
+
+                j += 1
+
+            i += 1
+
+        return millyInputValues, inputValues
+
+
+def convertMureToMilly(millyInputValues, inputValues):
+    data = []
+    width = len(millyInputValues.keys()) + 1
+    height = len(inputValues) + 1
+
+    for _ in range(height):
+        tmp = []
+        for _ in range(width):
+            tmp.append('')
+        data.append(tmp)
+
+    k = 0
+    for inputValue in inputValues:
+        data[k + 1][0] = inputValue
+        k += 1
+
+    i = 0
+    for millyState, inputValuesStateWithOut in millyInputValues.items():
+        data[0][i + 1] = millyState
+
+        j = 0
+        for inputValue, stateWithOut in inputValuesStateWithOut.items():
+            data[j + 1][i + 1] = stateWithOut
+            j += 1
+
+        i += 1
+
+    return data
+
+
+
 if __name__ == '__main__':
     fileNameRead = "data/read/Milly2.csv"
     fileNameWrite = "data/write/Milly2.csv"
 
-    inputValues, mureStates, millyStates, millyInputValue = readMilyFromCsv(fileNameRead)
+    # inputValues, mureStates, millyStates, millyInputValue = readMilyFromCsv(fileNameRead)
+    # data = convertMillyToMure(inputValues, mureStates, millyStates, millyInputValue)
+    # printFormattedDict(data)
+    # writeToCsv(fileNameWrite, data)
 
-    data = convertMillyToMure(inputValues, mureStates, millyStates, millyInputValue)
+    fileNameRead = "data/read/Mure1.csv"
+    fileNameWrite = "data/write/Mure1.csv"
 
+    millyInputValues, inputValues = readMureFromCsv(fileNameRead)
+    data = convertMureToMilly(millyInputValues, inputValues)
     printFormattedDict(data)
-
     writeToCsv(fileNameWrite, data)
